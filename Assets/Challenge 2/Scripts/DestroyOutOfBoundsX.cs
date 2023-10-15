91a8efee1fd9f4a6902b374fedf0ada377232511
chughes973 <146023719+chughes973@users.noreply.github.com>
2023-10-15 19:19:57 +0100

    Task 5
    Balls and Dog destroyed if outside of screen limits

--- Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class DestroyOutOfBoundsX : MonoBehaviour
 {
-    private float leftLimit = 30;
-    private float bottomLimit = -5;
+    private float leftLimit = -35; //limit to dog leaving screen
+    private float bottomLimit = 0; //limit to ball below ground
 
     // Update is called once per frame
     void Update()
     {
         // Destroy dogs if x position less than left limit
-        if (transform.position.x > leftLimit)
+        if (transform.position.x < leftLimit) // changed > to < so that dog is destroy when too far
         {
             Destroy(gameObject);
         } 
         // Destroy balls if y position is less than bottomLimit
-        else if (transform.position.z < bottomLimit)
+        else if (transform.position.y < bottomLimit) // changes z to y so that ball is destroyed when below y position 0
         {
             Destroy(gameObject);
         }
